7f5b2edd109f3747c99acf2c1f53da2205157313
meghan <meghan.duprey@gmail.com>
2018-12-10 08:08:29 -0500

    adding files

---
+++ js/magic.js
@@ -0,0 +1,19 @@
+const person = {
+  name:     'chris',
+  username: 'chrisoncode'
+};
+
+console.log(person.name);
+
+let myVar           = `blah ${person.name}`;
+const myNumber      = 3;
+const myStringggggg =  'here';
+
+function doSomething() {
+  return myVar;
+}
+
+doSomething();
+console.log(myVar);
+
+myVar = 'something else';
